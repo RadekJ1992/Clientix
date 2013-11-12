89725eded8ffde2c2c34aa65bdae2dcd68360fc6
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2013-11-12 12:39:55 +0100

    Można połączyć się z Cloudixem i wysłać pakiet

--- Clientix/Client.Designer.cs
+++ Clientix/Client.Designer.cs
@@ -25,19 +25,19 @@
         private void InitializeComponent() {
             this.enteredTextField = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.CloudIPField = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.CloudPortField = new System.Windows.Forms.TextBox();
-            this.ManagerPortField = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.ManagerIPField = new System.Windows.Forms.TextBox();
-            this.connectToCloud = new System.Windows.Forms.Button();
-            this.connectToManager = new System.Windows.Forms.Button();
+            this.connectToManagerButton = new System.Windows.Forms.Button();
             this.log = new System.Windows.Forms.TextBox();
             this.sendText = new System.Windows.Forms.Button();
             this.clientList = new System.Windows.Forms.CheckedListBox();
+            this.connectToCloudButton = new System.Windows.Forms.Button();
+            this.cloudIPField = new System.Windows.Forms.TextBox();
+            this.managerIPField = new System.Windows.Forms.TextBox();
+            this.cloudPortField = new System.Windows.Forms.TextBox();
+            this.managerPortField = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // enteredTextField
@@ -56,13 +56,6 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Tu wprowadź tekst wysyłany";
             // 
-            // CloudIPField
-            // 
-            this.CloudIPField.Location = new System.Drawing.Point(368, 25);
-            this.CloudIPField.Name = "CloudIPField";
-            this.CloudIPField.Size = new System.Drawing.Size(125, 20);
-            this.CloudIPField.TabIndex = 2;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -75,30 +68,16 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(368, 58);
+            this.label3.Location = new System.Drawing.Point(368, 51);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(108, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Port chmury kablowej";
             // 
-            // CloudPortField
-            // 
-            this.CloudPortField.Location = new System.Drawing.Point(373, 74);
-            this.CloudPortField.Name = "CloudPortField";
-            this.CloudPortField.Size = new System.Drawing.Size(120, 20);
-            this.CloudPortField.TabIndex = 5;
-            // 
-            // ManagerPortField
-            // 
-            this.ManagerPortField.Location = new System.Drawing.Point(373, 233);
-            this.ManagerPortField.Name = "ManagerPortField";
-            this.ManagerPortField.Size = new System.Drawing.Size(120, 20);
-            this.ManagerPortField.TabIndex = 9;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(368, 217);
+            this.label4.Location = new System.Drawing.Point(370, 182);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 13);
             this.label4.TabIndex = 8;
@@ -107,36 +86,21 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(368, 169);
+            this.label5.Location = new System.Drawing.Point(368, 143);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(62, 13);
             this.label5.TabIndex = 7;
             this.label5.Text = "IP zarządcy";
             // 
-            // ManagerIPField
+            // connectToManagerButton
             // 
-            this.ManagerIPField.Location = new System.Drawing.Point(368, 184);
-            this.ManagerIPField.Name = "ManagerIPField";
-            this.ManagerIPField.Size = new System.Drawing.Size(125, 20);
-            this.ManagerIPField.TabIndex = 6;
-            // 
-            // connectToCloud
-            // 
-            this.connectToCloud.Location = new System.Drawing.Point(373, 108);
-            this.connectToCloud.Name = "connectToCloud";
-            this.connectToCloud.Size = new System.Drawing.Size(119, 48);
-            this.connectToCloud.TabIndex = 10;
-            this.connectToCloud.Text = "Połącz z chmurą";
-            this.connectToCloud.UseVisualStyleBackColor = true;
-            // 
-            // connectToManager
-            // 
-            this.connectToManager.Location = new System.Drawing.Point(373, 271);
-            this.connectToManager.Name = "connectToManager";
-            this.connectToManager.Size = new System.Drawing.Size(119, 48);
-            this.connectToManager.TabIndex = 11;
-            this.connectToManager.Text = "Połącz z zarządcą";
-            this.connectToManager.UseVisualStyleBackColor = true;
+            this.connectToManagerButton.Location = new System.Drawing.Point(371, 224);
+            this.connectToManagerButton.Name = "connectToManagerButton";
+            this.connectToManagerButton.Size = new System.Drawing.Size(100, 44);
+            this.connectToManagerButton.TabIndex = 11;
+            this.connectToManagerButton.Text = "Połącz z zarządcą";
+            this.connectToManagerButton.UseVisualStyleBackColor = true;
+            this.connectToManagerButton.Click += new System.EventHandler(this.connectToManager);
             // 
             // log
             // 
@@ -146,7 +110,7 @@
             this.log.Name = "log";
             this.log.ReadOnly = true;
             this.log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.log.Size = new System.Drawing.Size(333, 232);
+            this.log.Size = new System.Drawing.Size(333, 176);
             this.log.TabIndex = 12;
             // 
             // sendText
@@ -157,38 +121,76 @@
             this.sendText.TabIndex = 13;
             this.sendText.Text = "Wyślij tekst do zaznaczonego klienta";
             this.sendText.UseVisualStyleBackColor = true;
+            this.sendText.Click += new System.EventHandler(this.sendMessage);
             // 
             // clientList
             // 
             this.clientList.FormattingEnabled = true;
-            this.clientList.Location = new System.Drawing.Point(504, 16);
+            this.clientList.Location = new System.Drawing.Point(482, 9);
             this.clientList.Name = "clientList";
-            this.clientList.Size = new System.Drawing.Size(145, 304);
+            this.clientList.Size = new System.Drawing.Size(142, 259);
             this.clientList.TabIndex = 14;
+            // 
+            // connectToCloudButton
+            // 
+            this.connectToCloudButton.Location = new System.Drawing.Point(371, 92);
+            this.connectToCloudButton.Name = "connectToCloudButton";
+            this.connectToCloudButton.Size = new System.Drawing.Size(100, 44);
+            this.connectToCloudButton.TabIndex = 19;
+            this.connectToCloudButton.Text = "Połącz z chmurą";
+            this.connectToCloudButton.UseVisualStyleBackColor = true;
+            this.connectToCloudButton.Click += new System.EventHandler(this.connectToCloud);
+            // 
+            // cloudIPField
+            // 
+            this.cloudIPField.Location = new System.Drawing.Point(371, 28);
+            this.cloudIPField.Name = "cloudIPField";
+            this.cloudIPField.Size = new System.Drawing.Size(100, 20);
+            this.cloudIPField.TabIndex = 20;
+            // 
+            // managerIPField
+            // 
+            this.managerIPField.Location = new System.Drawing.Point(371, 159);
+            this.managerIPField.Name = "managerIPField";
+            this.managerIPField.Size = new System.Drawing.Size(100, 20);
+            this.managerIPField.TabIndex = 21;
+            // 
+            // cloudPortField
+            // 
+            this.cloudPortField.Location = new System.Drawing.Point(371, 67);
+            this.cloudPortField.Name = "cloudPortField";
+            this.cloudPortField.Size = new System.Drawing.Size(100, 20);
+            this.cloudPortField.TabIndex = 22;
+            // 
+            // managerPortField
+            // 
+            this.managerPortField.Location = new System.Drawing.Point(371, 198);
+            this.managerPortField.Name = "managerPortField";
+            this.managerPortField.Size = new System.Drawing.Size(100, 20);
+            this.managerPortField.TabIndex = 23;
             // 
             // Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 337);
+            this.ClientSize = new System.Drawing.Size(636, 279);
+            this.Controls.Add(this.managerPortField);
+            this.Controls.Add(this.cloudPortField);
+            this.Controls.Add(this.managerIPField);
+            this.Controls.Add(this.cloudIPField);
+            this.Controls.Add(this.connectToCloudButton);
             this.Controls.Add(this.clientList);
             this.Controls.Add(this.sendText);
             this.Controls.Add(this.log);
-            this.Controls.Add(this.connectToManager);
-            this.Controls.Add(this.connectToCloud);
-            this.Controls.Add(this.ManagerPortField);
+            this.Controls.Add(this.connectToManagerButton);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.ManagerIPField);
-            this.Controls.Add(this.CloudPortField);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.CloudIPField);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.enteredTextField);
             this.Name = "Client";
             this.Text = "Client";
-            this.Load += new System.EventHandler(this.Client_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,19 +200,19 @@
 
         private System.Windows.Forms.TextBox enteredTextField;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox CloudIPField;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox CloudPortField;
-        private System.Windows.Forms.TextBox ManagerPortField;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox ManagerIPField;
-        private System.Windows.Forms.Button connectToCloud;
-        private System.Windows.Forms.Button connectToManager;
+        private System.Windows.Forms.Button connectToManagerButton;
         private System.Windows.Forms.TextBox log;
         private System.Windows.Forms.Button sendText;
         private System.Windows.Forms.CheckedListBox clientList;
+        private System.Windows.Forms.Button connectToCloudButton;
+        private System.Windows.Forms.TextBox cloudIPField;
+        private System.Windows.Forms.TextBox managerIPField;
+        private System.Windows.Forms.TextBox cloudPortField;
+        private System.Windows.Forms.TextBox managerPortField;
     }
 }
 
--- Clientix/Client.cs
+++ Clientix/Client.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,33 +17,112 @@ namespace Clientix {
 
     public partial class Client : Form {
 
+        private struct VPIVCI {
+            public int VPI;
+            public int VCI;
+        }
+
         //otrzymany i wysyłany pakiets
         private Packet.ATMPacket receivedPacket;
         private Packet.ATMPacket processedPacket;
 
+        private Queue<Packet.ATMPacket> packetsFromString;
+
+        //dane chmury
+        private IPAddress cloudAddress;        //Adres na którym chmura nasłuchuje
+        private Int32 cloudPort;           //port chmury
+
+        //dane zarządcy
+        private IPAddress managerAddress;        //Adres na którym chmura nasłuchuje
+        private Int32 managerPort;           //port chmury
+
+        private IPEndPoint cloudEndPoint;
+        private IPEndPoint managerEndPoint;
+
+        private Socket cloudSocket;
+        private Socket managerSocket;
+
+        private Thread receiveThread;     //wątek służący do odbierania połączeń
+        private Thread sendThread;        // analogicznie - do wysyłania
+
+        public bool isRunning { get; private set; }     //info czy klient chodzi - dla zarządcy
+
+        public bool isConnectedToCloud { get; private set; } // czy połączony z chmurą?
+        public bool isConnectedToManager { get; private set; } // czy połączony z zarządcą?
+        
         //unikalna nazwa klienta widziana przez zarządcę
         private String clientName;
 
-        //tablica innych węzłów klienckich podłączonych do zarządcy
+        //tablica innych węzłów klienckich podłączonych otrzymana do zarządcy
         private String[] otherClients;
 
         // tablica kierowania
-        private Dictionary<Packet.VpiVci, Packet.VpiVci> VCArray;
+        private Dictionary<VPIVCI,VPIVCI> VCArray;
 
-        public Client() {         
+        public Client() {
             InitializeComponent();
         }
 
         private void sendMessage(object sender, EventArgs e) {
-
+            packetsFromString = Packet.AAL.getATMPackets(enteredTextField.Text);
+            if (!isConnectedToCloud) log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") +
+                                                    " Not connected to cloud!");
+            else {
+                foreach (Packet.ATMPacket packet in packetsFromString) {
+                    //:DLA_PRZYKŁADU
+                    packet.VCI = 1;
+                    packet.VPI = 1;
+                    packet.port = 1;
+                    //
+                    log.AppendText("wysyłam pakiet!");
+                    Stream stream = new NetworkStream(cloudSocket);
+                    BinaryFormatter bformatter = new BinaryFormatter();
+                    bformatter.Serialize(stream, packet);
+                    stream.Close();
+                }
+            }
         }
+        
+        private void connectToCloud(object sender, EventArgs e) {
+            if (IPAddress.TryParse(cloudIPField.Text, out cloudAddress)) {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Cloud IP set properly as " + cloudAddress.ToString() + " \n");
+            }
+            else {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error reading cloud IP" + " \n");
+            }
+            if (Int32.TryParse(cloudPortField.Text, out cloudPort)) {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Cloud port set properly as " + cloudPort.ToString() + " \n");
+            }
+            else {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error reading cloud Port" + " \n");
+            }
 
-        private void chooseServerIPAndPort(object sender, EventArgs e) {
+            cloudSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
+            IPEndPoint endPoint = new IPEndPoint(cloudAddress, cloudPort);
+            try {
+                cloudSocket.Connect(endPoint);
+                isConnectedToCloud = true;
+            } catch (SocketException ex) {
+                isConnectedToCloud = false;
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error while connecting to cloud\n");
+                log.AppendText(DateTime.Now.ToString("Wrong IP or port?\n"));
+            }
         }
-
-        private void chooseManagerIPAndPort(object sender, EventArgs e) {
-
-        }
+        
+        private void connectToManager(object sender, EventArgs e) {
+            if (IPAddress.TryParse(managerIPField.Text, out managerAddress)) {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Manager IP set properly as " + managerAddress.ToString() + " \n");
+            }
+            else {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error reading manager IP" + " \n");
+            }
+            if (Int32.TryParse(managerPortField.Text, out managerPort)) {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Manager port set properly as " + managerPort.ToString() + " \n");
+            }
+            else {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error reading manager Port" + " \n");
+            }
+        }       
     }
 }
